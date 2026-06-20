07f6b19b76090c1ef59689d97473bdbffdfdb6dc
lyj404 <lyj404@qq.com>
2026-06-20 10:38:56 +0800

    修复指纹认证：MainActivity改为FlutterFragmentActivity

--- android/app/src/main/kotlin/com/keevault/keevault/MainActivity.kt
+++ android/app/src/main/kotlin/com/keevault/keevault/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.keevault.keevault
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
