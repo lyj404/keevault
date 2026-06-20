d6e5bfad79cb5215613a21dafef6ac08d1464682
lyj404 <lyj404@qq.com>
2026-06-20 11:03:56 +0800

    更新版本号至0.4.6

--- lib/features/about/screens/about_screen.dart
+++ lib/features/about/screens/about_screen.dart
@@ -13,7 +13,7 @@ class AboutScreen extends StatefulWidget {
 
 class _AboutScreenState extends State<AboutScreen> {
   static const String _appName = 'KeeVault';
-  static const String _version = '0.4.5';
+  static const String _version = '0.4.6';
   static const String _buildNumber = '1';
   static const String _githubUrl = 'https://github.com/lyj404/keevault';
   static const String _issuesUrl = 'https://github.com/lyj404/keevault/issues';
